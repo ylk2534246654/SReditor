20846657f5c92c4e00f864d6478b7478976f8ed7
ylk2534246654 <2534246654@qq.com>
2019-04-17 16:26:23 +0800

    优化编辑框颜色

--- app/src/main/java/com/yx/sreditor/MainActivity.java
+++ app/src/main/java/com/yx/sreditor/MainActivity.java
@@ -2,6 +2,8 @@ package com.yx.sreditor;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -23,9 +26,11 @@ import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout Layout_Design;
-    boolean show = false,Text_Color = false;
-    EditText EditText_Code;
+    private LinearLayout Layout_Design;
+    private boolean show = false,Text_Color = false;
+    private EditText EditText_Code;
+    private int Mcursor=0;
+    private String shuruqiandezhi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,26 +44,48 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // 输入的内容变化的监听
+
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // 输入前的监听
+                Mcursor = EditText_Code.getSelectionStart();
+                shuruqiandezhi = EditText_Code.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // 输入后的监听
+                if(EditText_Code.getSelectionStart()!=0){
+                    if(Mcursor > EditText_Code.getSelectionStart()){
+                        //Log.e("输入值",Mcursor+"/"+EditText_Code.getSelectionStart());
+                        String delete =shuruqiandezhi.substring(EditText_Code.getSelectionStart(),Mcursor);
+                        Log.e("删除值",delete);
+                        if(delete.indexOf("\"")!=-1 | delete.indexOf("=")!=-1 | delete.indexOf("<")!=-1 | delete.indexOf(">")!=-1| delete.length()>2){
+                            code_color();
+                        }
+                    }else if(Mcursor < EditText_Code.getSelectionStart()){
+                        String input_text = EditText_Code.getText().toString().substring(Mcursor,EditText_Code.getSelectionStart());
+                        Log.e("输入值",input_text);
+                        if(input_text.indexOf("\"")!=-1 | input_text.indexOf("=")!=-1 | input_text.indexOf("<")!=-1 | input_text.indexOf(">")!=-1| input_text.length()>2){
+                            code_color();
+                        }
+                    }
+                }
+
+
                 if(!Text_Color){//判断是否是非是手动输入
                     try {
                         //初始化代码颜色
-                        init_Code();
+                        //
+
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                     //每次监听完确认非手动输入关闭
                     Text_Color=false;
                 }
-            }
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-                // 输入前的监听
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // 输入后的监听
             }
         });
         //初始化界面
@@ -76,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             //读取本地文件
             EditText_Code.setText(file.read(this,"SmolarSystem.xml"));
+            code_color();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -110,79 +138,85 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     /**
      * 初始化代码颜色
      */
-    public void init_Code(){
-        String message = EditText_Code.getText().toString();
-        //获取光标位置
-        int cursor = EditText_Code.getSelectionStart();
-        //设置非手动输入
-        Text_Color = true;
 
-        SpannableStringBuilder style=new SpannableStringBuilder(message);
-        int int_1;
-        int int_2;
-        int int_color_0=0;;
-        //注释颜色
-        while (message.indexOf("<!--")!=-1 && message.indexOf("-->")!=-1){
-            style.setSpan(new ForegroundColorSpan(Color.rgb(180,180,180 )),message.indexOf("<!--"),message.indexOf("-->")+3,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-            String message2 = utils.substring(message,"<!--","-->");
-            message2 = "<!--" + message2 + "-->";
-            message = StringToSixthUtils.decode(StringToSixthUtils.encode(message).replaceFirst(StringToSixthUtils.encode(message2), StringToSixthUtils.encode(utils.text_null(message2.length()))));
-        }
-        //代码颜色
-        while (message.indexOf("<")<message.indexOf(">")){
-            String message2 = utils.substring(message,"<",">");
-            message2 = "<" + message2 + ">";
-            int_color_0 = message.indexOf("<");
-            if(message2.indexOf(" ")!=-1){
-                //<开头颜色
-                style.setSpan(new ForegroundColorSpan(Color.rgb(0,0,158)),int_color_0,int_color_0+message2.indexOf(" "),Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-                //项的颜色
-                if(message2.indexOf(" ")<message2.indexOf("=")){
-                    int_1 = 0;
-                    int_2 = message2.indexOf("=");
-                    for (int i=0;i<utils.appearNumber(message2,"=");i++){
-                        if(int_2>int_1 && int_1!=-1){
-                            int_1 = message2.indexOf(" ",int_1)+1;
-                            int_2 = message2.indexOf("=",int_2)+1;
-                            style.setSpan(new ForegroundColorSpan(Color.rgb(11,11,205)),int_color_0+int_1,int_color_0+int_2,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-                        }
-                    }
-                }
-                //值的颜色
-                if(message2.indexOf("\"")<message2.indexOf("\"",message2.indexOf("\"")+1)){
-                    int_1 = 0;
-                    int_2 = 1;
-                    for (int i=0;i<utils.appearNumber(message2,"\"");i++){
-                        if(int_2>int_1 && int_1!=-1){
-                            if(i==0){//第一次数据初始化
-                                int_1 = message2.indexOf("\"",0);
-                            }else {
-                                int_1 = message2.indexOf("\"",int_2+1);
-                            }
-                            int_2 = message2.indexOf("\"",int_1+1)+1;
-                            if(int_2>int_1 && int_1!=-1){
-                                style.setSpan(new ForegroundColorSpan(Color.rgb(0,128 ,0)),int_color_0+int_1,int_color_0+int_2,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-                            }
-                        }
-                    }
-                }
-            }else {
-                //>结尾颜色
-                style.setSpan(new ForegroundColorSpan(Color.rgb(	0,0,158 )),int_color_0,int_color_0+message2.length(),Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
-            }
-            try {
-                //替换
+    private void code_color() {
+        try{
+            String message = EditText_Code.getText().toString();
+            //获取光标位置
+            int cursor = EditText_Code.getSelectionStart();
+            //设置非手动输入
+            Text_Color = true;
+
+            SpannableStringBuilder style=new SpannableStringBuilder(message);
+            int int_1;
+            int int_2;
+            int int_color_0=0;;
+            //注释颜色
+            while (message.indexOf("<!--")!=-1 && message.indexOf("-->")!=-1){
+                style.setSpan(new ForegroundColorSpan(Color.rgb(180,180,180 )),message.indexOf("<!--"),message.indexOf("-->")+3, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+                String message2 = utils.substring(message,"<!--","-->");
+                message2 = "<!--" + message2 + "-->";
                 message = StringToSixthUtils.decode(StringToSixthUtils.encode(message).replaceFirst(StringToSixthUtils.encode(message2), StringToSixthUtils.encode(utils.text_null(message2.length()))));
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+            //代码颜色
+            while (message.indexOf("<")<message.indexOf(">")){
+                String message2 = utils.substring(message,"<",">");
+                message2 = "<" + message2 + ">";
+                int_color_0 = message.indexOf("<");
+                if(message2.indexOf(" ")!=-1){
+                    //<开头颜色
+                    style.setSpan(new ForegroundColorSpan(Color.rgb(0,0,158)),int_color_0,int_color_0+message2.indexOf(" "),Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+                    //项的颜色
+                    if(message2.indexOf(" ")<message2.indexOf("=")){
+                        int_1 = 0;
+                        int_2 = message2.indexOf("=");
+                        for (int i=0;i<utils.appearNumber(message2,"=");i++){
+                            if(int_2>int_1 && int_1!=-1){
+                                int_1 = message2.indexOf(" ",int_1)+1;
+                                int_2 = message2.indexOf("=",int_2)+1;
+                                style.setSpan(new ForegroundColorSpan(Color.rgb(11,11,205)),int_color_0+int_1,int_color_0+int_2,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+                            }
+                        }
+                    }
+                    //值的颜色
+                    if(message2.indexOf("\"")<message2.indexOf("\"",message2.indexOf("\"")+1)){
+                        int_1 = 0;
+                        int_2 = 1;
+                        for (int i=0;i<utils.appearNumber(message2,"\"");i++){
+                            if(int_2>int_1 && int_1!=-1){
+                                if(i==0){//第一次数据初始化
+                                    int_1 = message2.indexOf("\"",0);
+                                }else {
+                                    int_1 = message2.indexOf("\"",int_2+1);
+                                }
+                                int_2 = message2.indexOf("\"",int_1+1)+1;
+                                if(int_2>int_1 && int_1!=-1){
+                                    style.setSpan(new ForegroundColorSpan(Color.rgb(0,128 ,0)),int_color_0+int_1,int_color_0+int_2,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+                                }
+                            }
+                        }
+                    }
+                }else {
+                    //>结尾颜色
+                    style.setSpan(new ForegroundColorSpan(Color.rgb(	0,0,158 )),int_color_0,int_color_0+message2.length(),Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+                }
+                try {
+                    //替换
+                    message = StringToSixthUtils.decode(StringToSixthUtils.encode(message).replaceFirst(StringToSixthUtils.encode(message2), StringToSixthUtils.encode(utils.text_null(message2.length()))));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            EditText_Code.setText(style);
+            //置光标位置
+            EditText_Code.setSelection(cursor);
+            Text_Color=false;
+        }catch (Exception e){
+
         }
-        EditText_Code.setText(style);
-        //置光标位置
-        EditText_Code.setSelection(cursor);
     }
 }
